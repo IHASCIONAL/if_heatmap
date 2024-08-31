506bbfdd0d45e3dd4de9b57e07af817f71bc0f7d
Ismael <ismaelhenriqueeu@gmail.com>
2024-08-31 11:27:46 -0300

    build: Adicionando aba para o futuro mapa de calor por praça

--- src/frontend.py
+++ src/frontend.py
@@ -1,5 +1,28 @@
 import streamlit as st
 
+class Forms:
+
+    def __init__(self):
+        self.estado = None
+
+    def define_state(self):
+        self.estado = st.selectbox(label="Selecione a praça de interesse para ver o mapa de calor", options=["Rio de Janeiro", "SP"])
+    
+    def form_title(self, title):
+        st.header(title)
+
+    def submit_button(self, name):
+        return st.form_submit_button(name)
+        
+    def create_forms(self, title):
+        with st.form("form"):
+            self.form_title(title)
+            self.define_state()
+            submitted = self.submit_button("Ver Mapa")
+
+            if submitted:
+                st.write(f"Praça selecionada: {self.estado}")
+
 class ExcelValidatorUI:
 
     def __init__(self):
---
+++ src/pages/02_mapa_de_calor.py
@@ -0,0 +1,12 @@
+from frontend import Forms
+
+formulario = Forms()
+
+def main():
+    formulario.create_forms("Mapa de Calor")
+
+
+if __name__ == "__main__":
+    main()
+
+                      
